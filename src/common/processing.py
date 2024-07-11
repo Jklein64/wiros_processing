45af0edd0d5f3f932588801efc6c3148bae982df
Jason Klein <jklein.64@verizon.net>
2024-07-11 13:39:55 -0700

    Correctly convert Wifi message to numpy array

--- src/common/processing.py
+++ src/common/processing.py
@@ -7,17 +7,22 @@ from rf_msgs.msg import Wifi
 
 def array_from_wifi_message(msg: Wifi):
     """
-    Converts the given ROS `Wifi` message `msg` into a numpy array with shape `(n_sub, n_rows, n_cols)`, where the first dimension corresponds to receivers and the second to transmitters.
+    Converts the given ROS `Wifi` message `msg` into a numpy array with shape `(n_sub, n_rx, n_tx)`
     """
     bw = msg.bw * 1e6  # msg.bw is in MHz
     n_sub = msg.n_sub
     n_rows = msg.n_rows
     n_cols = msg.n_cols
-    csi_shape = (n_sub, n_rows, n_cols)
-    # extract raw csi matrix
-    csi_real = np.array(msg.csi_real).reshape(csi_shape)
-    csi_imag = np.array(msg.csi_imag).reshape(csi_shape)
-    csi = csi_real + 1j * csi_imag.astype(np.complex128)
+
+    # extract csi matrix (copied from WiROS code)
+    csi = np.asarray(msg.csi_real) + 1.0j * np.asarray(msg.csi_imag)
+    # note: the Wifi message format stores CSI data with a Fortran-style ordering instead of the
+    # C-style ordering that numpy uses by default. As documented by np.isfortran, the transpose
+    # of an array with Fortran-style ordering is an array with C-style ordering. By reversing
+    # the order of the dimensions in the reshape and then taking the transpose, this line of
+    # code will correctly read the Fortran-style ordered data in a way that allows for C-style
+    # modification prevalent in numpy.
+    csi = csi.reshape((n_cols, n_rows, n_sub)).T
 
     # these subcarriers are rotated. need to find justification for why this is the case
     if bw == 80e6:
